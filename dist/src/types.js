e62228a4288edd8ca8e3a7ffde55eebc5e32a189
Kyle Murphy <kmurph73@gmail.com>
2021-03-10 11:06:46 -0800

    glob
    and dont include dist while at it

--- dist/src/types.js
+++
@@ -1,7 +0,0 @@
-export var GroupState;
-(function (GroupState) {
-    GroupState[GroupState["Begun"] = 0] = "Begun";
-    GroupState[GroupState["WithinCode"] = 1] = "WithinCode";
-    GroupState[GroupState["Donezo"] = 2] = "Donezo";
-})(GroupState || (GroupState = {}));
-//# sourceMappingURL=types.js.map
--- src/createDoctests.ts
+++ src/createDoctests.ts
@@ -1,15 +1,19 @@
-import fs from "fs";
 import { parseFiles } from "./parseFiles.js";
 import { writeTests } from "./writeTests.js";
 
 import { myCompact } from "./util.js";
+import path from "path";
+import fg from "fast-glob";
 
-const jsOrTsX = /\.(js|ts)x?$/;
+// const jsOrTsX = /\.(js|ts)x?$/;
 
 export const createDoctests = async (dir: string): Promise<void> => {
-  const files = fs.readdirSync(dir).map((file) => {
-    return jsOrTsX.test(file) ? `${dir}/${file}` : null;
-  });
+  // const files = fs.readdirSync(dir).map((file) => {
+  //   return jsOrTsX.test(file) ? `${dir}/${file}` : null;
+  // });
+
+  const glob = path.join(dir, "**/*.js");
+  const files = await fg(glob);
 
   const groups = parseFiles(myCompact(files));
   writeTests(groups);
