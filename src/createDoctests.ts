b057bc90183d66e44c0eab38fd9239e5518501da
Kyle Murphy <kmurph73@gmail.com>
2021-03-10 11:10:21 -0800

    cleanup

--- src/createDoctests.ts
+++ src/createDoctests.ts
@@ -5,13 +5,7 @@ import { myCompact } from "./util.js";
 import path from "path";
 import fg from "fast-glob";
 
-// const jsOrTsX = /\.(js|ts)x?$/;
-
 export const createDoctests = async (dir: string): Promise<void> => {
-  // const files = fs.readdirSync(dir).map((file) => {
-  //   return jsOrTsX.test(file) ? `${dir}/${file}` : null;
-  // });
-
   const glob = path.join(dir, "**/*.js");
   const files = await fg(glob);
 
