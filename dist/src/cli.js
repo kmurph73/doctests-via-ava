d8015ea45fca76bb77f4363fcf8f4dbee7269e66
Kyle Murphy <kmurph73@gmail.com>
2022-07-23 15:42:13 -0700

    nada

--- dist/src/cli.js
+++ dist/src/cli.js
@@ -3,7 +3,7 @@ import { createDoctests } from "./createDoctests.js";
 const glob = process.argv[2];
 const tsOpt = process.argv[3];
 if (glob == null || glob.trim() === "") {
-    console.error('you must pass in a glob to doctests-via-ava eg `yarn doctests-via-ava "./src"`');
+    console.error("you must pass in a glob to doctests-via-ava eg `yarn doctests-via-ava src/**/*.js`");
 }
 else if (tsOpt != null && tsOpt !== "--ts") {
     console.error(`a third argument was passed in of ${tsOpt}, which should be "--ts", or nothing`);
--- src/cli.ts
+++ src/cli.ts
@@ -7,7 +7,7 @@ const tsOpt = process.argv[3];
 
 if (glob == null || glob.trim() === "") {
   console.error(
-    'you must pass in a glob to doctests-via-ava eg `yarn doctests-via-ava "./src"`'
+    "you must pass in a glob to doctests-via-ava eg `yarn doctests-via-ava src/**/*.js`"
   );
 } else if (tsOpt != null && tsOpt !== "--ts") {
   console.error(
